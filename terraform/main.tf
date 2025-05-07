c0b5d527bbc5cfd721f137e93712e25aed2f958c
Carole FOBASSO <77639832+Carlie0310@users.noreply.github.com>
2025-05-07 12:37:21 +0200

    Update main.tf
    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,33 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
   skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
   endpoints {
     ec2 = "http://ip10-0-14-4-d0dgra805akh4glkf930-4566.direct.lab-boris.fr"
+    iam = "http://ip10-0-14-4-d0dgra805akh4glkf930-4566.direct.lab-boris.fr"
+    sts = "http://ip10-0-14-4-d0dgra805akh4glkf930-4566.direct.lab-boris.fr"
+  }
+}
+
+# Generate a new pseudo-AMI ID on every apply (using the current timestamp)
+resource "random_id" "ami_trigger" {
+  byte_length = 2
+
+  # Keepers forces regeneration each run by changing this value
+  keepers = {
+    run_timestamp = timestamp()
   }
 }
 
 resource "aws_instance" "demo" {
-  ami           = "ami-12345678"
+  count         = var.instance_count
+  # Use a random AMI string so Terraform sees a change each commit
+  ami           = "ami-${random_id.ami_trigger.hex}"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "LocalStackDemo-${count.index + 1}"
+  }
 }
